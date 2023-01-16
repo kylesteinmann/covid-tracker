02902387b555d8531e0f6b4476175b0b346778e8
kylesteinmann <105984447+kylesteinmann@users.noreply.github.com>
2023-01-16 10:31:57 -0600

    new work station

---
+++ lib/covid/tracker.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "tracker/version"
+
+module Covid
+  module Tracker
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
